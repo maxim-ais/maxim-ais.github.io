c0b5d5b8fcc66ea03ef5f4f9653b10b3275a8648
Maksim_Tkachou <m.tkachou@aisnovations.com>
2019-12-03 20:52:39 +0300

    test

---
+++ js/cookie.js
@@ -0,0 +1,27 @@
+function setCookie(name, value, options) {
+    const initialOptions = {
+        ...options,
+        path: '/',
+    };
+
+    if (options.expires instanceof Date && options.expires.toUTCString) {
+        options.expires = options.expires.toUTCString();
+    }
+
+    let updatedCookie = `${encodeURIComponent(name)}=${value}`;
+
+    for (let optionKey in initialOptions) {
+        if (initialOptions.hasOwnProperty(optionKey)) {
+            updatedCookie += `; ${optionKey}`;
+            let optionValue = initialOptions[optionKey];
+            if (optionValue) {
+                updatedCookie += `=${optionValue}`
+            }
+        }
+    }
+
+    document.cookie = updatedCookie;
+}
+const expires = new Date();
+expires.setFullYear(expires.getFullYear() + 1);
+setCookie('test', 'hhh', {domain: `www.${window.location.hostname}`, expires});
